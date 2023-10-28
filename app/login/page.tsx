92bc3e5a0720c7b082b71592b515260e0b579dbc
Den <90708440+dklymovych@users.noreply.github.com>
2023-10-28 22:12:39 +0300

    add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,54 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+export default function LoginPage() {
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          borderRadius: '5px',
+          boxShadow: '1px 1px 1px 1px gray',
+          padding: '30px'
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+            size='small'
+            margin="normal"
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+          />
+          <TextField
+            size='small'
+            margin="normal"
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
